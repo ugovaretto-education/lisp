390a47c478914208535f3894e170aa4123209f63
ugovaretto <ugovaretto@gmail.com>
2024-10-26 23:31:38 +0800

    Ch 14

---
+++ common-lisp-book/ch14.lisp
@@ -0,0 +1,27 @@
+(defvar name 'fred)
+
+(defvar sentence `(this is ,name from pittsburgh))
+
+(defvar sentence-2 `(I gave ,name about ,(* 25 8) dollars))
+
+(defmacro simple-incf (var &optional (amount 1))
+           (list 'setq var (list '+ var amount)))
+
+(defmacro simpler-incf (var &optional (amount 1))
+           `(setq ,var (+ ,var ,amount)))
+
+(defmacro simple-rotatef (x y)
+  `(let* ((tmp ,y))
+     (setf ,y ,x)
+     (setf ,x tmp)))
+
+(defmacro showvar (x)
+  '(format t "~&The value of ~A is ~A" (symbol-name 'x) x ))
+
+(defmacro set-mutual (x y)
+  `(setf x y))
+
+(defmacro set-zero (&rest variables)
+  `(progn ,@(mapcar #'(lambda (var)
+                        (list 'setf var 0))
+                    variables)'(zeroed ,@variables)))
