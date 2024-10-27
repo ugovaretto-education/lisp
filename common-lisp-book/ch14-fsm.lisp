21dfdc99cee3a2d573e9a960277c2542e04d5890
ugovaretto <ugovaretto@gmail.com>
2024-10-27 22:04:51 +0800

    Finished FSM

--- common-lisp-book/ch14-fsm.lisp
+++ common-lisp-book/ch14-fsm.lisp
@@ -31,7 +31,7 @@
 
 (defvar *nodes* ())
 (defvar *arcs* ())
-(defvar *current-node* ())
+(defvar *current-node* nil)
 
 (defun initialize ()
   (setf *nodes* nil)
@@ -50,30 +50,68 @@
       (error "No node names ~A exists." name)))
 
 (defun find-arc (from to)
-   (or (find-if (lambda (arc)
+   (find-if (lambda (arc)
               (let
                   ((nf (node-name (arc-from arc)))
                    (nt (node-name (arc-to arc))))
                 (and (eq nf from) (eq nt to))))
-            *arcs*)
-       (error "Cannot find Arc going from ~A to ~A" from to)))
+            *arcs*))
 
 
 (defmacro defarc (from label to &optional action)
   `(add-arc ',from ',label ',to ',action))
 
 (defun add-arc (from-name label to-name action)
-  (let* ((from (find-node from-name))
-         (to (find-node to-name))
-         (new-arc (make-arc :from from
-                            :label label
-                            :to to
-                            :action action)))
-    (setf *arcs* (nconc *arcs* (list new-arc)))
-    (setf (node-outputs from)
-          (nconc (node-outputs from)
-                 (list new-arc)))
-    (setf (node-inputs to)
-          (nconc (node-inputs to)
-                 (list new-arc)))
-    new-arc))
+  (let ((a (find-arc from-name to-name)))
+    (if a a
+        (let* ((from (find-node from-name))
+               (to (find-node to-name))
+               (new-arc (make-arc :from from
+                                  :label label
+                                  :to to
+                                  :action action)))
+          (push *arcs* new-arc)
+          (push (node-outputs from) new-arc)
+          (push (node-inputs to) new-arc)
+          new-arc))))
+
+(defun fsm (&optional (starting-state 'start))
+  (initialize)
+  (init-states)
+  (setf *current-node* (find-node starting-point))
+  (do (nil)
+      ((null (node-outputs *current-node*)))
+    (one-transition)))
+
+(defun one-transition ()
+  (format t "~&Current state: ~A, action: " (node-name *current-node*))
+  (let* ((act (read))
+         (arc (find act (node-outputs *current-node*) :key #'arc-label)))
+    (setf *current-node* (arc-to arc))))
+
+(defun init-states()
+  (defnode start)
+  (defnode have-5)
+  (defnode have-10)
+  (defnode have-15)
+  (defnode have-20)
+  (defnode end)
+  (defarc start nickel have-5 "Clunk!")
+  (defarc start dime have-10 "Clink!")
+  (defarc start coin-return start "Nothing to return.")
+  (defarc have-5 nickel have-10 "Clunk!")
+  (defarc have-5 dime have-15 "Clink!")
+  (defarc have-5 coin-return start "Returned five cents.")
+  (defarc have-10 nickel have-15 "Clunk!")
+  (defarc have-10 dime have-20 "Clink!")
+  (defarc have-10 coint-return start "Returned ten cents.")
+  (defarc have-15 nickel have-20 "Clunk!")
+  (defarc have-15 dime have-20 "Nickel change.")
+  (defarc have-15 gum-button end "Deliver gum.")
+  (defarc have-15 coin-return start "Returned fifteen cents.")
+  (defarc have-20 nickel have-20 "Nickel returned.")
+  (defarc have-20 dime have-20 "Dime returned.")
+  (defarc have-20 gum-button end
+    "Deliver gum, nickel change.")
+  (defarc have-20 mint-button end "Deliver mints.")
+  (defarc have-20 coin-return start "Returned twenty cents."))
